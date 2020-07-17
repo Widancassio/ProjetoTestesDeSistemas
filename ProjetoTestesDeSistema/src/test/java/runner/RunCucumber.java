4d58679519ec710d42aebaced205e717387c5d2b
w.galindo.da.silva <w.galindo.da.silva@CPX-0GBHYXSJ5PM.dir.svc.accenture.com>
2020-07-17 00:13:14 -0300

    Novas classe de step definitions adicionadas a glue

--- ProjetoTestesDeSistema/src/test/java/runner/RunCucumber.java
+++ ProjetoTestesDeSistema/src/test/java/runner/RunCucumber.java
@@ -5,10 +5,9 @@ import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-		features = {"src/test/resources/features"},
+@CucumberOptions(features = { "src/test/resources/features" },
 
-		glue = {"stepDefinitions"},
+		glue = { "newsroomStepDefinitions", "buscaStepDefinitions", "suporteStepDefinitions", "lojaStepDefinitions" },
 
 		plugin = { "pretty" },
 
