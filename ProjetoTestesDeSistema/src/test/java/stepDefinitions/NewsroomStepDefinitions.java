6a1e17671ae00f3e07160e609bb37d77a2a9abf2
w.galindo.da.silva <w.galindo.da.silva@CPX-0GBHYXSJ5PM.dir.svc.accenture.com>
2020-07-17 00:14:46 -0300

    Nova classe de step definitions para a funcionalidades de Newsroom

---
+++ ProjetoTestesDeSistema/src/test/java/stepDefinitions/NewsroomStepDefinitions.java
@@ -0,0 +1,48 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class NewsroomStepDefinitions {
+
+	@Given("o usuario acesse a pagina de Newsroom")
+	public void o_usuario_acesse_a_pagina_de_Newsroom() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("o usuario realiza algum filtro")
+	public void o_usuario_realiza_algum_filtro() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("o usuario clica no botao resetar")
+	public void o_usuario_clica_no_botao_resetar() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("os filtros serao resetados")
+	public void os_filtros_serao_resetados() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("serao exibidos todos os topicos")
+	public void serao_exibidos_todos_os_topicos() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("o botao resetar sera removido da tela")
+	public void o_botao_resetar_sera_removido_da_tela() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+
+	
+}
