9c8b289d66cc8af56d82f4a21738a1b3f2eb5865
w.galindo.da.silva <w.galindo.da.silva@CPX-0GBHYXSJ5PM.dir.svc.accenture.com>
2020-07-17 00:15:25 -0300

    Nova classe de step definitions para a funcionalidade de Suporte

---
+++ ProjetoTestesDeSistema/src/test/java/stepDefinitions/SuporteStepDefinitions.java
@@ -0,0 +1,96 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class SuporteStepDefinitions {
+
+	@Given("o usuario acessa o site {string}")
+	public void o_usuario_acessa_o_site(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("usuario clica no botao suporte")
+	public void usuario_clica_no_botao_suporte() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("clica no icone iPhone")
+	public void clica_no_icone_iPhone() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("o usuario clica no botao Manual do usuario do iPhone")
+	public void o_usuario_clica_no_botao_Manual_do_usuario_do_iPhone() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("e exibido o manual do iPhone.")
+	public void e_exibido_o_manual_do_iPhone() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("o usuario acessa {string}")
+	public void o_usuario_acessa(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("seleciona o produto {string}")
+	public void seleciona_o_produto(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("usuario clica no link Alterar produto")
+	public void usuario_clica_no_link_Alterar_produto() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("seleciona o novo produto")
+	public void seleciona_o_novo_produto() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("tela de suporte do novo produto e exibida")
+	public void tela_de_suporte_do_novo_produto_e_exibida() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("o usuario acessa a area de suporte")
+	public void o_usuario_acessa_a_area_de_suporte() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("clica no botao Centro de Servico Autorizado Apple \\(AASP)")
+	public void clica_no_botao_Centro_de_Servico_Autorizado_Apple_AASP() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("clica no botao servico e suporte")
+	public void clica_no_botao_servico_e_suporte() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("e exibida a mensagem {string}")
+	public void e_exibida_a_mensagem(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+
+	
+}
