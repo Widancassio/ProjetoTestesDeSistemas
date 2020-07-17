167b7eda3f5a94ca9ffdd624a66e07b66d296a36
w.galindo.da.silva <w.galindo.da.silva@CPX-0GBHYXSJ5PM.dir.svc.accenture.com>
2020-07-17 00:14:18 -0300

    Nova classe de step definitions para a funcionalidade de Loja

---
+++ ProjetoTestesDeSistema/src/test/java/stepDefinitions/LojaStepDefinitions.java
@@ -0,0 +1,107 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LojaStepDefinitions {
+
+	@Given("o usuario acessa a Sacola de compras")
+	public void o_usuario_acessa_a_Sacola_de_compras() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("exista um produto na Sacola")
+	public void exista_um_produto_na_Sacola() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("usuario clicar no link Remover")
+	public void usuario_clicar_no_link_Remover() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("e exibida tela com a mensagem {string}")
+	public void e_exibida_tela_com_a_mensagem(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("o usuario acesse o produto {string}")
+	public void o_usuario_acesse_o_produto(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("selecione o produto {string}")
+	public void selecione_o_produto(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("usuario clicar no botao Comprar")
+	public void usuario_clicar_no_botao_Comprar() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("clicar no botao Colocar na sacola")
+	public void clicar_no_botao_Colocar_na_sacola() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("tela e exibida com o Produto selecionado.")
+	public void tela_e_exibida_com_o_Produto_selecionado() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("selecionar as informacoes do produto")
+	public void selecionar_as_informacoes_do_produto() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("nao escolher a {string}")
+	public void nao_escolher_a(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("o botao ?Colocar na sacola? nao e exibido")
+	public void o_botao_Colocar_na_sacola_nao_e_exibido() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("o usuario acesse a Sacola de compras")
+	public void o_usuario_acesse_a_Sacola_de_compras() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("o usuario alterar a quantidade desejada do produto")
+	public void o_usuario_alterar_a_quantidade_desejada_do_produto() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("o valor total da compra e alterado")
+	public void o_valor_total_da_compra_e_alterado() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("a quantidade de itens da Sacola da navbar e alterada.")
+	public void a_quantidade_de_itens_da_Sacola_da_navbar_e_alterada() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+	
+}
