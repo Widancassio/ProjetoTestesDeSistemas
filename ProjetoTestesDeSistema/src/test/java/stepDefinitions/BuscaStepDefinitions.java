6448bffaf4f101152ed51a26c2df6ac865009ed4
w.galindo.da.silva <w.galindo.da.silva@CPX-0GBHYXSJ5PM.dir.svc.accenture.com>
2020-07-17 00:13:50 -0300

    Nova classe de step definitions para a funcionalidade Busca

---
+++ ProjetoTestesDeSistema/src/test/java/stepDefinitions/BuscaStepDefinitions.java
@@ -0,0 +1,36 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class BuscaStepDefinitions {
+
+	@Given("o usuario acessa o campo de busca")
+	public void o_usuario_acessa_o_campo_de_busca() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("o usuario clica no link Encontre uma loja")
+	public void o_usuario_clica_no_link_Encontre_uma_loja() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("clica em lista completa de lojas")
+	public void clica_em_lista_completa_de_lojas() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("e exibida a lista de lojas do brasil")
+	public void e_exibida_a_lista_de_lojas_do_brasil() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+
+	
+}
