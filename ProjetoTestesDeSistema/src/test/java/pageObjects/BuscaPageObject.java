58f0c01a094d3e6e7e2e16289fb7b087c2f9c794
ewertton.souza <ewertton.souza@MVRCSIS-N04799.mvrec.local>
2020-07-19 22:02:10 -0300

    alterações do page objective de busca

--- ProjetoTestesDeSistema/src/test/java/pageObjects/BuscaPageObject.java
+++ ProjetoTestesDeSistema/src/test/java/pageObjects/BuscaPageObject.java
@@ -5,14 +5,12 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import auxiliares.MetodosAuxiliares;
 
 public class BuscaPageObject {
 
 	private WebDriver driver;
-	private WebDriverWait wait;
 	private String pageUrl = "https://www.apple.com/br/";
 	private List<WebElement> lojas;
 	private MetodosAuxiliares metValida = new MetodosAuxiliares();
